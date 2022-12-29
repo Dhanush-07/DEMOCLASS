ec8e0afcf4d40ca8bc7171ddf2ef06197d656207
Dhanush <2000031114@kluniversity.in>
2022-12-29 15:55:34 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BindingComponent } from './binding/binding.component';
+import { FormsComponent } from './forms/forms.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
+  {
+    path : "forms",
+    component:FormsComponent
+  },
   {
     path : "binding",
     component:BindingComponent
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { BindingComponent } from './binding/binding.component';
+import { FormsComponent } from './forms/forms.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BindingComponent } from './binding/binding.component';
     LoginComponent,
     RegisterComponent,
     HomeComponent,
-    BindingComponent
+    BindingComponent,
+    FormsComponent
   ],
   imports: [
     BrowserModule,
